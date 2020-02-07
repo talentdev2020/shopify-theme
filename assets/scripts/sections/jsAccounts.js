3a1d07197d9c18786cf16cf95005dc50b2ce9e65
Laza <laza.presta@gmail.com>
2020-02-07 18:14:59 +0100

    Account, Login and Reset
    initial

--- assets/scripts/sections/jsAccounts.js
+++ assets/scripts/sections/jsAccounts.js
@@ -3,11 +3,15 @@ Shopify.theme.jsAccounts = {
 
     $('.js-recover-password').on('click', function(){
       $('#login').hide();
+      $('.login__image__element').hide();
+      $('.reset__image__element').show();
       $('#recover').show();
     });
 
     $('.cancel-recover-password').on('click', function(){
       $('#recover').hide();
+      $('.reset__image__element').hide();
+      $('.login__image__element').show();
       $('#login').show();
     })
 
--- assets/z__jsAccounts.js
+++ assets/z__jsAccounts.js
@@ -4,10 +4,14 @@ Shopify.theme.jsAccounts = {
   init: function init($section) {
     $('.js-recover-password').on('click', function () {
       $('#login').hide();
+      $('.login__image__element').hide();
+      $('.reset__image__element').show();
       $('#recover').show();
     });
     $('.cancel-recover-password').on('click', function () {
       $('#recover').hide();
+      $('.reset__image__element').hide();
+      $('.login__image__element').show();
       $('#login').show();
     });
   },
