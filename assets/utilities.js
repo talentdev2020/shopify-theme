782f745011047e9ada6b493e71a775883e71c1a1
Francis Bowen <frankbowen0912@gmail.com>
2020-02-13 08:55:53 -0800

    product-recharge app section

--- assets/utilities.js
+++ assets/utilities.js
@@ -1056,6 +1056,7 @@ Shopify.theme.responsiveVideo = {
 };
 
 var selectCallback = function selectCallback(variant, selector) {
+  console.log("selectCallback 11");
   var $product = $('.product-' + selector.product.id);
   var $notify_form = $('.notify-form-' + selector.product.id);
   var $productForm = $('.product_form, .shopify-product-form', $product);
@@ -1068,6 +1069,51 @@ var selectCallback = function selectCallback(variant, selector) {
 
   if (variant) {
     var notifyMessage = Shopify.translation.email_content + variant.name + ' | ' + notifyUrl + '?variant=' + variant.id;
+  }
+
+  console.log(variant);
+
+  if (variant) {
+    var meal_one = document.getElementById('meals_one');
+    var meal_subscribe = document.getElementById('meals_subscribe');
+
+    if (meal_one == null) {
+      console.log('meal_one = null');
+    }
+
+    if (meal_subscribe == null) {
+      console.log('meal_subscribe = null');
+    }
+
+    var length = variant.options.length;
+
+    for (var i = 0; i < length; i++) {
+      if (variant.options[i]) {
+        switch (variant.options[i]) {
+          case "6-Pack":
+            {
+              meal_one.innerHTML = 2;
+              meal_subscribe.innerHTML = 2;
+              $("input[name='option-" + i + "'][value=" + variant.options[i] + "]").prop("checked", true);
+              $("input[name='option-" + i + "1'][value=" + variant.options[i] + "]").prop("checked", true);
+              $("input[name='option-" + i + "2'][value=" + variant.options[i] + "]").prop("checked", true);
+            }
+            break;
+
+          case "12-Pack":
+            {
+              meal_one.innerHTML = 3;
+              meal_subscribe.innerHTML = 3;
+              $("input[name='option-" + i + "'][value=" + variant.options[i] + "]").prop("checked", true);
+              $("input[name='option-" + i + "1'][value=" + variant.options[i] + "]").prop("checked", true);
+              $("input[name='option-" + i + "2'][value=" + variant.options[i] + "]").prop("checked", true);
+            }
+            break;
+
+          default:
+        }
+      }
+    }
   }
 
   if ($notifyFormInputs.hasClass('customer--true')) {
