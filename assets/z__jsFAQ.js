ca3e039993c345d5bba0f25bf24c92b95cb67ab6
Francis Bowen <frankbowen0912@gmail.com>
2020-02-12 20:18:34 -0800

    Product - Thumbnail contents

--- assets/z__jsFAQ.js
+++ assets/z__jsFAQ.js
@@ -3,9 +3,11 @@
 Shopify.theme.jsFAQ = {
   init: function init() {
     var $faqHeading = $('.faq-accordion > .faq-contents > .groups dt > button');
-    $('.faq-accordion > .faq-contents > .groups > dd').attr('aria-hidden', true);
+    console.log("clicked");
+    $('.faq-accordion > .faq-contents > .groups > .dd').attr('aria-hidden', true);
     $faqHeading.attr('aria-expanded', false);
     $faqHeading.on('click activate', function () {
+      console.log("click activate");
       var faqIcons = $(this).find('.icon');
       var state = $(this).attr('aria-expanded') === 'false' ? true : false;
 
@@ -19,14 +21,14 @@ Shopify.theme.jsFAQ = {
 
           if (!currentstate) {
             faq_this.attr('aria-expanded', false);
-            faq_this.parent().next().slideToggle(function () {
+            faq_this.next().slideToggle(function () {
               var faqIcons1 = faq_this.find('.icon');
 
               if (faqIcons1.hasClass('icon--active')) {
                 faqIcons1.toggleClass('icon--active');
               }
             });
-            faq_this.parent().next().attr('aria-hidden', true);
+            faq_this.next().attr('aria-hidden', true);
           }
         };
 
@@ -36,12 +38,12 @@ Shopify.theme.jsFAQ = {
       }
 
       $(this).attr('aria-expanded', state);
-      $(this).parent().next().slideToggle(function () {
+      $(this).next().slideToggle(function () {
         if (faqIcons.hasClass('icon--active')) {
           faqIcons.toggleClass('icon--active');
         }
       });
-      $(this).parent().next().attr('aria-hidden', !state);
+      $(this).next().attr('aria-hidden', !state);
       return false;
     });
     $faqHeading.on('keydown', function (event) {
